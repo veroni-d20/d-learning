06d2c093d19e53acd8084f32e0d1ac2b8feee98c
veroni-d20 <veronishwetha.23it@licet.ac.in>
2022-05-10 01:14:13 +0530

    basic template for courses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyCourses from "./pages/MyCourses";
+import Retrieve from "./pages/Retrieve";
 import Upload from "./pages/Upload";
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/upload" element={<Upload />} />
+        <Route path="/getCourses" element={<Retrieve />} />
+        <Route path="/myCourses" element={<MyCourses />} />
       </Routes>
     </BrowserRouter>
   );
