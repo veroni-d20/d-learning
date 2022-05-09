d428e1b4de69366144714707e37ad79665ad2b41
Subhiksha18vic <esubivictoria129@gmail.com>
2022-05-10 01:36:45 +0530

    Basic Login Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Upload from "./pages/Upload";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/upload" element={<Upload />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
