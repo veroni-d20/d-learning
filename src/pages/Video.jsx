bfd0651c637dd925fba34e69819463edd249b469
RAKSHA V G <vgraksha26@gmail.com>
2022-05-11 00:36:53 +0530

    update UI

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -3,6 +3,7 @@ import ReactPlayer from "react-player";
 import { useMoralis } from "react-moralis";
 import { useNavigate } from "react-router-dom";
 import { Button, Typography } from "@mui/material";
+import bg from "../images/bg.png";
 
 export default function Video() {
   const { Moralis } = useMoralis();
@@ -14,42 +15,58 @@ export default function Video() {
     navigate("/");
   };
   return (
-    <div className="vh-100 p-4">
-      <div className="d-flex justify-content-end">
-        <Button
-          onClick={(e) => {
-            navigate("/getCourses");
+    <>
+      <section
+        className="d-flex flex-column align-items-center justify-content-center vh-100"
+        style={{
+          background: `url("${bg}")no-repeat center/cover`,
+        }}
+      >
+        <div
+          className="bg-white align-items-center justify-content-center h-75"
+          style={{
+            minHeight: "85vh",
+            width: "85%",
+            borderRadius: "10px",
+            boxShadow:
+              "0 8px 16px 0 rgba(0, 0, 0, 0.15), 0 6px 20px 0 rgba(0, 0, 0, 0.16)",
           }}
         >
-          Get Courses
-        </Button>
-        <Typography component="h2" variant="h5">
-          |
-        </Typography>
-        <Button onClick={logout}>Logout</Button>
-      </div>
-      <div className="container d-flex flex-column  my-4">
-        <h3>Agile Methodology</h3>
-        <hr></hr>
-        <ReactPlayer
-          url="https://www.youtube.com/watch?v=zsjvFFKOm3c"
-          muted={false}
-          volume={true}
-          playing={false}
-          controls={true}
-          width="100%"
-        />
-        <div className="d-flex justify-content-between rounded-2-bottom py-4">
-          <div>
-            <h5>Learn how to use agile methods to develop software</h5>
-          </div>
-          <div>
-            <p>
-              <b>4</b> Weeks
-            </p>
+          <div className="container d-flex flex-column p-md-4 p-3">
+            <div className="d-flex justify-content-end">
+              <Button
+                onClick={(e) => {
+                  navigate("/getCourses");
+                }}
+              >
+                Get Courses
+              </Button>
+              <Typography component="h2" variant="h5">
+                |
+              </Typography>
+              <Button onClick={logout}>Logout</Button>
+            </div>
+            <h3 className="m-0">Agile Methodology</h3>
+            <hr></hr>
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=zsjvFFKOm3c"
+              muted={false}
+              volume={true}
+              playing={false}
+              controls={true}
+              width="100%"
+            />
+            <div className="d-flex justify-content-between pt-3">
+              <h5 className="m-0">
+                Learn how to use agile methods to develop software
+              </h5>
+              <p className="m-0">
+                <b>4</b> Weeks
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 }
